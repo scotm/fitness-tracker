d6b459de883a85c6c5678dc4298ad224e063607e
Scott Macdonald <scott.scotm@gmail.com>
2025-02-16 03:27:17 +0000

    adding info to the next.config.js so it can load images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+	images: {
+		remotePatterns: [
+			{ hostname: "tailwindui.com" },
+		],
+	},
+};
 
 export default config;
