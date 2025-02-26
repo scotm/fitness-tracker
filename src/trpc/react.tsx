b2aec798c05175115635840374b8e9dd78f28e9d
Scott Macdonald <scott.scotm@gmail.com>
2025-02-26 01:02:52 +0000

    chore: Update React Query dependencies and add DevTools
    - Upgraded @tanstack/react-query to version 5.66.9
    - Added @tanstack/react-query-devtools
    - Integrated ReactQueryDevtools component in TRPCReactProvider

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -6,6 +6,7 @@ import { createTRPCReact } from "@trpc/react-query";
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 import { useState } from "react";
 import SuperJSON from "superjson";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import type { AppRouter } from "~/server/api/root";
 import { createQueryClient } from "./query-client";
@@ -65,6 +66,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
 			<api.Provider client={trpcClient} queryClient={queryClient}>
 				{props.children}
 			</api.Provider>
+			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	);
 }
